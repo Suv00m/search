9e37c6d7dd26061f4c3e32a1530f73ed99760505
suv00m <shuvammandal131@gmail.com>
2026-02-22 17:17:51 +0530

    feat: added a regex support

--- src/main.rs
+++ src/main.rs
@@ -9,12 +9,14 @@ struct Cli {
     pattern: String,
     /// The path to the file to read
     path: std::path::PathBuf,
+    /// the regex flag
+    #[arg(short = 'F', long)]
+    fixed: bool,
 }
 
 fn main() -> Result<()> {
     let args = Cli::parse();
-    
-    search::find_match(&args.pattern, &args.path)
-        .context("could not find match")?;
+
+    search::find_match(&args.pattern, &args.path, args.fixed).context("could not find match")?;
     Ok(())
 }
--- src/search/mod.rs
+++ src/search/mod.rs
@@ -1,21 +1,36 @@
-use std::io::{BufRead, BufReader, BufWriter, Write};
-use std::fs::File;
-use std::path::Path;
 use anyhow::{Context, Result};
+use regex::Regex;
+use std::fs::File;
+use std::io::{BufRead, BufReader, BufWriter, Write};
+use std::path::Path;
 
-pub fn find_match(pattern: &str, path: &Path) -> Result<()> {
-    let file = File::open(path)
-        .with_context(|| format!("could not open file '{}'", path.display()))?;
+pub fn find_match(pattern: &str, path: &Path, fixed: bool) -> Result<()> {
+    let file =
+        File::open(path).with_context(|| format!("could not open file '{}'", path.display()))?;
     let content_reader = BufReader::new(file);
-    
+
+    enum SearchMode {
+        Plain(String),
+        Regex(Regex),
+    }
     let stdout = std::io::stdout();
     let mut writer = BufWriter::new(stdout.lock());
+    let mode = if !fixed {
+        SearchMode::Regex(Regex::new(pattern)?)
+    } else {
+        SearchMode::Plain(pattern.to_string())
+    };
+
     for line in content_reader.lines() {
         let line = line.context("could not read line")?;
-        if line.contains(pattern) {
+        let is_match = match &mode {
+            SearchMode::Plain(text) => line.contains(text.as_str()),
+            SearchMode::Regex(re) => re.is_match(&line),
+        };
+        if is_match {
             writeln!(writer, "{}", line)?;
         }
     }
+
     Ok(())
 }
-
